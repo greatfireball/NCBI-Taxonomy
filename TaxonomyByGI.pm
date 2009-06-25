fc4a0f0ba3fb9bf3bdd27847c0bab67017f3cad8
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2009-06-25 08:10:53 +0000

    Version 0.2
    Ich habe jetzt eine neue Funktion eingebaut, damit ich auch nur
    bestimmte Ränge zurückliefern kann. Außerdem habe ich use strict und use
    warnings eingebaut und deshalb noch einige Anpassungen machen müssen.
    
    
    git-svn-id: file:///home/frf53jh/repo/TaxonomyByGI/trunk@364 1f6d2edc-b336-0410-a9ba-a3136dcd62bc

--- TaxonomyByGI.pm
+++ TaxonomyByGI.pm
@@ -1,5 +1,8 @@
 package TaxonomyByGI;
 
+use strict;
+use warnings;
+
 # written by Frank Förster
 
 # allows to get the linnage for a given set of GIs
@@ -7,6 +10,7 @@ package TaxonomyByGI;
 my $TAXDIR = '/bio/data/NCBI/nt/taxonomy'; # where are the taxonomy-files stored
 
 # Version 0.1 24.06.2009
+# Version 0.2 25.06.2009
 
 #
 # Command: getLineagesbyGI( ref to list of gis, type )
@@ -27,8 +31,7 @@ sub getLineagesbyGI(\@$) {
     my @gilist = @{shift()};
     my $seqtype = uc(shift);
 
-    die "Use of [Nn] for nucleotides or [Pp] for p
-roteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
+    die "Use of [Nn] for nucleotides or [Pp] for proteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
 
     my %gilist2search = ();
     # check if gis are only numbers!
@@ -39,6 +42,85 @@ roteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
 
     return check4gis(\%gilist2search, $seqtype);
 
+}
+
+# Command: getTaxonomicRankbyGI( ref to list of gis, type, taxonomic rank )
+#
+# ref to list of gis ... should be selfexplaining
+# type ... [NnPp] for nuleotides or proteins
+# taxonomic rank ... a string which determined for what rank is searched for (e.g. 'family')
+#
+# result is a reference of a hash of hash
+#        first hash key are the GIs
+#        hash keys for the elements
+#                sciname -> scientific name of taxon
+#                rank -> rank given by NCBI
+#                taxid -> taxid resulting from GI
+#
+# Note all ranks used by NCBI are
+#
+# "class","family","forma","genus","infraclass","infraorder","kingdom","no rank","order",
+# "parvorder","phylum","species","species group","species subgroup","subclass","subfamily",
+# "subgenus","subkingdom","suborder","subphylum","subspecies","subtribe","superclass",
+# "superfamily","superkingdom","superorder","superphylum","tribe","varietas"
+
+sub getTaxonomicRankbyGI(\@$$) {
+
+    my @gilist = @{shift()};
+    my $seqtype = uc(shift);
+    my $taxonrank = shift;
+
+    # check for used rank!
+    my $taxonrankvalid = 0;
+    foreach (getallranksusedbyNCBI()) {
+	if ($_ eq $taxonrank) {
+	    $taxonrankvalid = 1;
+	    last;
+	}
+    }
+
+    die "Use of not known taxonomic rank\n" if ($taxonrankvalid == 0);
+    die "Use of [Nn] for nucleotides or [Pp] for proteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
+
+    my %gilist2search = ();
+    # check if gis are only numbers!
+    foreach (@gilist) {
+	die "Use only numbers as GIs!" if ($_ =~ /\D/);
+	$gilist2search{$_}++;
+    }
+
+    my $result = check4gis(\%gilist2search, $seqtype);
+
+    # now I need to extract the right taxon rank
+    my %taxonomicrankbyGI = ();
+    foreach my $gi (keys %{$result}) {
+	foreach my $act_level (@{$result->{$gi}}) {
+	    if ($act_level->{rank} eq $taxonrank) {
+		# I found the rank, so add to resulthash and go to next GI!
+		$taxonomicrankbyGI{$gi} = $act_level;
+		last;
+	    }
+	}
+	# at this position the question is, Did I found a rank? If not make an empty hash-entry
+	$taxonomicrankbyGI{$gi} = {} if (!exists $taxonomicrankbyGI{$gi});
+    }
+
+    return \%taxonomicrankbyGI;
+
+}
+
+# sub getallranksusedbyNCBI
+#
+# return a list of all ranks used by NCBI obtained by the following code:
+# cut -f 3 -d "|" nodes.dmp | sed -e 's/^[[:blank:]]*\([^[:blank:]]*\)[[:blank:]]*$/"\1"/g' | sort | uniq | tr "\n" ","
+#
+
+sub getallranksusedbyNCBI {
+    return ("class","family","forma","genus","infraclass","infraorder","kingdom",
+	    "no rank","order","parvorder","phylum","species","species group",
+	    "species subgroup","subclass","subfamily","subgenus","subkingdom",
+	    "suborder","subphylum","subspecies","subtribe","superclass",
+	    "superfamily","superkingdom","superorder","superphylum","tribe","varietas");
 }
 
 sub check4gis(%$) {
@@ -74,7 +156,6 @@ sub check4gis(%$) {
     close(FH);
 
     # now I can perform the mapping
-    $i=0;
     my %Lineage_by_gi = ();
     foreach my $gi (keys %taxid_found_by_gi) {
 	my $taxid = $taxid_found_by_gi{$gi};
@@ -100,14 +181,14 @@ sub check4gis(%$) {
         my @tmp = split(/\t\|\t/, $_ );
 	next if ($tmp[3] !~ /scientific name/);
 	next if (!exists $taxidnamesneeded{$tmp[0]}); # next if I do not need the name
-	print STDERR "Doppelbelegung von $tmp[0]" if (defined $names[$tmp[0]]);
-        $names{$tmp[0]} = $tmp[1];
+	print STDERR "Doppelbelegung von $tmp[0]" if (defined $names_by_taxid{$tmp[0]});
+        $names_by_taxid{$tmp[0]} = $tmp[1];
     }
     close(FH);
 
     # last step is to add the scientific names to the lineage
     foreach my $gi (keys %Lineage_by_gi) {
-	foreach (@{$Lineage_by_gi{$gi}}) { $_->{sciname} = $names{$_->{taxid}}; }
+	foreach (@{$Lineage_by_gi{$gi}}) { $_->{sciname} = $names_by_taxid{$_->{taxid}}; }
     }
 
     return \%Lineage_by_gi;
--- test_TaxonomyByGI.pl
+++ test_TaxonomyByGI.pl
@@ -6,6 +6,7 @@ my @gis = (18361509,18361510, 18361511,18361512, 18361513,18361514);
 
 use Data::Dumper;
 
-my $a=TaxonomyByGI::getLineagesbyGI(@gis, "N");
+#my $a=TaxonomyByGI::getLineagesbyGI(@gis, "N");
+my $a=TaxonomyByGI::getTaxonomicRankbyGI(@gis, "N", 'family');
 
 print Dumper($a);
