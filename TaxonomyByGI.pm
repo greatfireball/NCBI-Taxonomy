6f53adbb2b7d035f1896c482f37515e73889d689
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2009-06-26 00:14:25 +0000

    Version 0.3 Ich habe jetzt das ganze Mapping auf eine neue Methode umgestellt, indem ich ein anderes Format in der tmp-Datei eingestellt habe. Das hat zwar zur Folge, dass die Datei von 2.3 GB auf 5.2 GB vergrößert wird, aber sie enthält dazu auch gleich noch die Proteine und der Zugriff ist um Größenordnungen beschleunigt, da jede Zeile eine feste Länge hat.
    Außerdem lade ich jetzt named.dmp und nodes.dmp beim Laden des Modules.
    Im Moment liefern die beiden Ladefunktionen noch den Hash oder das Array
    zurück, dass sollte ich demnächst noch in eine Referenz ändern.
    
    In zukünftigen Versionen will ich die bytesperline-Werte beim ersten
    Einlesen dynamisch ermitteln werde.
    
    
    git-svn-id: file:///home/frf53jh/repo/TaxonomyByGI/trunk@365 1f6d2edc-b336-0410-a9ba-a3136dcd62bc

--- TaxonomyByGI.pm
+++ TaxonomyByGI.pm
@@ -8,15 +8,18 @@ use warnings;
 # allows to get the linnage for a given set of GIs
 
 my $TAXDIR = '/bio/data/NCBI/nt/taxonomy'; # where are the taxonomy-files stored
+my @nodes = getnodesimported();            # import the nodes.dmp for later use at loading of the module
+my %names_by_taxid = getnamesimported();   # import the names.dmp for later use at loading of the module
 
 # Version 0.1 24.06.2009
 # Version 0.2 25.06.2009
+# Version 0.3 26.06.2009
+#       no protein/nucleic acid parameter is nessesary
 
 #
-# Command: getLineagesbyGI( ref to list of gis, type )
+# Command: getLineagesbyGI( ref to list of gis )
 #
 # ref to list of gis ... should be selfexplaining
-# type ... [NnPp] for nuleotides or proteins
 #
 # result is a reference of a hash of list of hash
 #        first hash key are the GIs
@@ -26,12 +29,9 @@ my $TAXDIR = '/bio/data/NCBI/nt/taxonomy'; # where are the taxonomy-files stored
 #                rank -> rank given by NCBI
 #                taxid -> taxid resulting from GI
 
-sub getLineagesbyGI(\@$) {
+sub getLineagesbyGI(\@) {
 
     my @gilist = @{shift()};
-    my $seqtype = uc(shift);
-
-    die "Use of [Nn] for nucleotides or [Pp] for proteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
 
     my %gilist2search = ();
     # check if gis are only numbers!
@@ -40,14 +40,13 @@ sub getLineagesbyGI(\@$) {
 	$gilist2search{$_}++;
     }
 
-    return check4gis(\%gilist2search, $seqtype);
+    return check4gis(\%gilist2search);
 
 }
 
-# Command: getTaxonomicRankbyGI( ref to list of gis, type, taxonomic rank )
+# Command: getTaxonomicRankbyGI( ref to list of gis, taxonomic rank )
 #
 # ref to list of gis ... should be selfexplaining
-# type ... [NnPp] for nuleotides or proteins
 # taxonomic rank ... a string which determined for what rank is searched for (e.g. 'family')
 #
 # result is a reference of a hash of hash
@@ -64,10 +63,9 @@ sub getLineagesbyGI(\@$) {
 # "subgenus","subkingdom","suborder","subphylum","subspecies","subtribe","superclass",
 # "superfamily","superkingdom","superorder","superphylum","tribe","varietas"
 
-sub getTaxonomicRankbyGI(\@$$) {
+sub getTaxonomicRankbyGI(\@$) {
 
     my @gilist = @{shift()};
-    my $seqtype = uc(shift);
     my $taxonrank = shift;
 
     # check for used rank!
@@ -80,7 +78,6 @@ sub getTaxonomicRankbyGI(\@$$) {
     }
 
     die "Use of not known taxonomic rank\n" if ($taxonrankvalid == 0);
-    die "Use of [Nn] for nucleotides or [Pp] for proteins as type!\n" unless ($seqtype eq "N" || $seqtype eq "P");
 
     my %gilist2search = ();
     # check if gis are only numbers!
@@ -89,7 +86,7 @@ sub getTaxonomicRankbyGI(\@$$) {
 	$gilist2search{$_}++;
     }
 
-    my $result = check4gis(\%gilist2search, $seqtype);
+    my $result = check4gis(\%gilist2search);
 
     # now I need to extract the right taxon rank
     my %taxonomicrankbyGI = ();
@@ -123,36 +120,25 @@ sub getallranksusedbyNCBI {
 	    "superfamily","superkingdom","superorder","superphylum","tribe","varietas");
 }
 
-sub check4gis(%$) {
-    my ($gilist, $type) = @_;
+sub check4gis(\%) {
+    my ($gilist) = @_;
 
     my %taxid_found_by_gi = ();
 
-    if ($type eq "N") {
-	open(FH, "<".$TAXDIR."/gi_taxid_nucl.dmp");
-    } else {
-	open(FH, "<".$TAXDIR."/gi_taxid_prot.dmp");
+    open(FH, "<".$TAXDIR."/tmp.txt");
+    binmode(FH);
+
+    my $bytesperline = 23;
+    my $tmp = "";
+
+    foreach my $gi (keys %$gilist) {
+	my $bytepos = ($gi-1)*$bytesperline;
+	seek(FH, $bytepos, 0);
+	read(FH, $tmp, $bytesperline);
+	my @splitted_line = split(/\t/, $tmp);
+	$taxid_found_by_gi{$gi} = int($splitted_line[1]);
     }
 
-    while (<FH>) {
-	my @tmp = split(/\t/, $_ );
-	chomp(@tmp);
-	if (exists $gilist->{$tmp[0]}) {
-	    # put the gi and corresponding taxid into new list and delete it from 2search list
-	    delete($gilist->{$tmp[0]});
-       	    $taxid_found_by_gi{$tmp[0]} = $tmp[1];
-	    last if (keys %{$gilist} == 0);
-	}
-    }
-    close(FH);
-
-    # now I have to read the nodes.dmp
-    my @nodes = ();
-    open(FH, "<".$TAXDIR."/nodes.dmp");
-    while (<FH>) {
-	my @tmp = split(/\t\|\t/, $_ );
-	$nodes[$tmp[0]] = {ancestor => int($tmp[1]), rank => $tmp[2]};
-    }
     close(FH);
 
     # now I can perform the mapping
@@ -160,7 +146,6 @@ sub check4gis(%$) {
     foreach my $gi (keys %taxid_found_by_gi) {
 	my $taxid = $taxid_found_by_gi{$gi};
 	$Lineage_by_gi{$gi} = ();
-#	while ($nodes[$taxid]->{ancestor} != 1) {
 	while () {
 	    push(@{$Lineage_by_gi{$gi}}, {taxid => $taxid, rank => $nodes[$taxid]->{rank}});
 	    $taxid = $nodes[$taxid]->{ancestor};
@@ -174,17 +159,6 @@ sub check4gis(%$) {
     foreach my $gi (keys %Lineage_by_gi) {
 	foreach (@{$Lineage_by_gi{$gi}}) { $taxidnamesneeded{$_->{taxid}}++ }
     }
-    # now I am able to search for the taxids
-    my %names_by_taxid = ();
-    open(FH, "<".$TAXDIR."/names.dmp");
-    while (<FH>) {
-        my @tmp = split(/\t\|\t/, $_ );
-	next if ($tmp[3] !~ /scientific name/);
-	next if (!exists $taxidnamesneeded{$tmp[0]}); # next if I do not need the name
-	print STDERR "Doppelbelegung von $tmp[0]" if (defined $names_by_taxid{$tmp[0]});
-        $names_by_taxid{$tmp[0]} = $tmp[1];
-    }
-    close(FH);
 
     # last step is to add the scientific names to the lineage
     foreach my $gi (keys %Lineage_by_gi) {
@@ -194,3 +168,32 @@ sub check4gis(%$) {
     return \%Lineage_by_gi;
 
 }
+
+sub getnodesimported {
+    # I want to read the nodes.dmp
+    my @nodes = ();
+    open(FH, "<".$TAXDIR."/nodes.dmp");
+    while (<FH>) {
+	my @tmp = split(/\t\|\t/, $_ );
+	$nodes[$tmp[0]] = {ancestor => int($tmp[1]), rank => $tmp[2]};
+    }
+    close(FH);
+
+    return @nodes;
+}
+
+sub getnamesimported {
+    my %names_by_taxid = ();
+    open(FH, "<".$TAXDIR."/names.dmp");
+    while (<FH>) {
+        my @tmp = split(/\t\|\t/, $_ );
+	next if ($tmp[3] !~ /scientific name/);
+	print STDERR "Doppelbelegung von $tmp[0]" if (defined $names_by_taxid{$tmp[0]});
+        $names_by_taxid{$tmp[0]} = $tmp[1];
+    }
+    close(FH);
+
+    return %names_by_taxid;
+}
+
+1;
