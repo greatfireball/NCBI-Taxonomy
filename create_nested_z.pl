dd01f7fd16e06d500ed210a1cec03afdac172d27
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2009-09-20 09:57:26 +0000

    Eine neue Version vom create_nested_z. Dabei nutze ich einen rekursiven Ansatz und kann damit die Laufzeit dramatisch verringern. So habe ich jetzt bis zur fertigen Erstellung nur noch 17 Sekunden Laufzeit.
    git-svn-id: file:///home/frf53jh/repo/NCBI-Taxonomy/trunk@468 1f6d2edc-b336-0410-a9ba-a3136dcd62bc

--- create_nested_z.pl
+++ create_nested_z.pl
@@ -33,75 +33,114 @@ while (<FH>)
 } 
 close(FH) || die "Can not close $data_filename";
 
-foreach (keys %data) {
-    push(@{$parent2index{$data{$_}->{parent}}}, $_);
-}
-
 print "Finished import of ".(keys %data)." node data\n";
 
 # now I want to create the tree! Root is node 1... I can just add it or better... Extract the root node:
 my ($rootnode) = grep { $names{$_} eq "root" } (keys %names);
-print "Nodenumber for root-node: $rootnode\n";
+print "Nodenumber for root-node: $rootnode\nStarting nested set generation...\n";
 
-# set the root to the first element:
-push(@nested_set_data, { 
-			id        => int($rootnode),
-			parent_id => $data{$rootnode}{parent},
-			rank      => $data{$rootnode}{rank},
-			name      => $names{$rootnode},
-			lft       => 1,
-			rgt       => 2
+foreach (keys %data) {
+    push(@{$parent2index{$data{$_}->{parent}}}, $_) if ($_ != $rootnode);
+    push(@nested_set_data, { 
+			id        => int($_),
+			parent_id => $data{$_}{parent},
+			rank      => $data{$_}{rank},
+			name      => $names{$_},
+			lft       => 0,
+			rgt       => 0
 		       });
-$id2index{$rootnode} = 0;
-
-# next step have to the insertion of the next level so the question
-# is, what are the ids for the next number or the other way arount,
-# which nodes have the $rootnode as parent
-
-my @nodes2goonwith = grep { ($data{$_}{parent} == $rootnode) && ($_ != $rootnode) } (keys %data);
-
-print "Found ".@nodes2goonwith." nodes in the second level\nStarting tree generation...\n".time()."\n";
-
-while (@nodes2goonwith) {
-
-    if (@nested_set_data % 1000 == 0) {
-	print time()."\tNested set contains ".@nested_set_data." Nodes\n";
-    }
-    my $act_id = shift(@nodes2goonwith);
-    insert_taxid( {
-		   id        => int($act_id),
-		   parent_id => $data{$act_id}{parent},
-		   rank      => $data{$act_id}{rank},
-		   name      => $names{$act_id},
-		   lft       => 0,
-		   rgt       => 0
-		  } );
+    $id2index{$_} = @nested_set_data - 1;
 }
+
+print "Nodes of nested set were generated\nStarting walk through the tree...\n";
+# now create the nested set using a recursive approach
+
+our $runnumber = 0;
+
+make_node($rootnode);
+
+print "Finished nested set generation!\nNumber of leaf-nodes: ".(grep {$_->{lft} + 1 == $_->{rgt}} @nested_set_data)."\n";
+
+# just a Test: Tardigrada:
+my ($searchset) = grep {$_->{name} eq "Tardigrada"} @nested_set_data;
+print Dumper(grep {$_->{lft} >= $searchset->{lft} && $_->{rgt} <= $searchset->{rgt}} @nested_set_data);
+use Data::Dumper;
+
+# another test with Milnesium tardigradum
+($searchset) = grep {$_->{name} eq "Milnesium tardigradum"} @nested_set_data;
+my @result = map {$_->{name}} sort {$a->{lft} <=> $b->{lft}} grep {$_->{lft} <= $searchset->{lft} && $_->{rgt} >= $searchset->{lft}} @nested_set_data;
+print Dumper(\@result);
 
 exit;
 
-sub insert_taxid {
-    my ($dataset) = @_;
+sub make_node ($) {
+    my ($nodenumber) = @_;
+    
+    $runnumber++;
+    $nested_set_data[$id2index{$nodenumber}]{lft} = $runnumber;
 
-    my $rgt2search = $nested_set_data[$id2index{$dataset->{parent_id}}]->{rgt};
-
-    foreach (@nested_set_data) {
-	if ($_->{rgt} > $rgt2search)
-	{
-	    $_->{lft} += 2;
-	    $_->{rgt} += 2;
-	}
-	elsif ($_->{rgt} == $rgt2search) {
-	    $_->{rgt} += 2;
-	}
+    if (defined $parent2index{$nodenumber}) {
+	# es sind noch unterliegende Knoten vorhanden
+	foreach (@{$parent2index{$nodenumber}}) { make_node($_); }
     }
-
-    $dataset->{rgt} = $rgt2search + 1;
-    $dataset->{lft} = $rgt2search;
-
-    push(@nested_set_data, $dataset);
-    $id2index{$dataset->{id}} = @nested_set_data - 1;
-    push(@nodes2goonwith, @{$parent2index{$dataset->{id}}} ) if (defined $parent2index{$dataset->{id}});
-
+    # wir sind am Blatt angekommen... rgt setzten und zurück
+    $runnumber++;
+    $nested_set_data[$id2index{$nodenumber}]{rgt} = $runnumber;
+    #	print "Found leaf... RGT: $runnumber\n";
     return;
 }
+
+# # set the root to the first element:
+
+
+# # next step have to the insertion of the next level so the question
+# # is, what are the ids for the next number or the other way arount,
+# # which nodes have the $rootnode as parent
+
+# my @nodes2goonwith = grep { ($data{$_}{parent} == $rootnode) && ($_ != $rootnode) } (keys %data);
+
+# print "Found ".@nodes2goonwith." nodes in the second level\nStarting tree generation...\n".time()."\n";
+
+# while (@nodes2goonwith) {
+
+#     if (@nested_set_data % 1000 == 0) {
+# 	print time()."\tNested set contains ".@nested_set_data." Nodes\n";
+#     }
+#     my $act_id = shift(@nodes2goonwith);
+#     insert_taxid( {
+# 		   id        => int($act_id),
+# 		   parent_id => $data{$act_id}{parent},
+# 		   rank      => $data{$act_id}{rank},
+# 		   name      => $names{$act_id},
+# 		   lft       => 0,
+# 		   rgt       => 0
+# 		  } );
+# }
+
+# exit;
+
+# sub insert_taxid {
+#     my ($dataset) = @_;
+
+#     my $rgt2search = $nested_set_data[$id2index{$dataset->{parent_id}}]->{rgt};
+
+#     foreach (@nested_set_data) {
+# 	if ($_->{rgt} > $rgt2search)
+# 	{
+# 	    $_->{lft} += 2;
+# 	    $_->{rgt} += 2;
+# 	}
+# 	elsif ($_->{rgt} == $rgt2search) {
+# 	    $_->{rgt} += 2;
+# 	}
+#     }
+
+#     $dataset->{rgt} = $rgt2search + 1;
+#     $dataset->{lft} = $rgt2search;
+
+#     push(@nested_set_data, $dataset);
+#     $id2index{$dataset->{id}} = @nested_set_data - 1;
+#     push(@nodes2goonwith, @{$parent2index{$dataset->{id}}} ) if (defined $parent2index{$dataset->{id}});
+
+#     return;
+# }
