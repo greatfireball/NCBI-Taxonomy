d855e497ac38778e0342e0d3d7bb3f93243d4e14
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2012-11-29 15:40:30 +0000

    Diese Version benutze ich jetzt im Moment, um meine GI-taxid Datenbank zu erstellen.
    git-svn-id: file:///home/frf53jh/repo/NCBI-Taxonomy/trunk@1851 1f6d2edc-b336-0410-a9ba-a3136dcd62bc

--- make_gi_taxid.pl
+++ make_gi_taxid.pl
@@ -4,9 +4,164 @@ use strict;
 use warnings;
 use Fcntl;
 
+### Initialization Section
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($INFO); 
+
+use Getopt::Long;
+
+my $linenumberreport = 10000000;
+
+my $gi_taxid_nucl = "gi_taxid_nucl.dmp";
+my $gi_taxid_prot = "gi_taxid_prot.dmp";
+my $outfile = "gi_taxid.bin";
+
+my $overwrite = 0;
+
+my $getopt_result = GetOptions(
+    'nucl=s' => \$gi_taxid_nucl,
+    'prot=s' => \$gi_taxid_prot,
+    'output=s' => \$outfile,
+    'overwrite!' => \$overwrite
+    );
+
+### Get a logger
+my $logger = get_logger();
+
+$logger->info("Started update process...");
+
+# check if the output file exists
+if (-e $outfile && $overwrite != 1)
+{
+    $logger->logdie("The outputfile exists! Remove it, use the parameter --overwrite, or give another output location using --outfile parameter");
+}
+
+# check if the inputfiles are valid
+if (! -e $gi_taxid_nucl)
+{
+    $logger->logdie("The inputfile for nucleotid gis to taxid is not valid. Used filename was '$gi_taxid_nucl'. Please give a valid filename using the --nucl parameter");
+}
+
+if (! -e $gi_taxid_prot)
+{
+    $logger->logdie("The inputfile for protein gis to taxid is not valid. Used filename was '$gi_taxid_prot'. Please give a valid filename using the --prot parameter");
+}
+
+
+# try to open the output file for writing
+open(OF, ">", $outfile) || $logger->logdie("Unable to open the outputfile '$outfile' for writing");
+
+# try to open the input files for reading
+open(NF, "<", $gi_taxid_nucl) || $logger->logdie("Unable to open the nucleotid input file '$gi_taxid_nucl' for reading");
+open(PF, "<", $gi_taxid_prot) || $logger->logdie("Unable to open the protein input file '$gi_taxid_prot' for reading");
+
+### define the currently used data format
+my $data_format = "LL";   ## this means I will store 2 32-bit values!
+my $empty_line = pack($data_format, (0,0));
+my $data_line_length = length($empty_line);
+
+my $run = 1;
+
+my $nucl_line = undef;
+my $prot_line = undef;
+my $output_line = 0;
+
+my ($n_gi, $n_taxid, $p_gi, $p_taxid);
+
+my $prot_line_number = 0;
+my $nucl_line_number = 0;
+
+my $out = "";
+
+while ($run)
+{
+    # check if we have to read a new line from nucleotide input
+    if (! $nucl_line)
+    {
+	$nucl_line = <NF>;
+	$nucl_line_number++ if ($nucl_line);
+
+	if ($nucl_line =~ /^(\d+)\t(\d+)/)
+	{ 
+	    ($n_gi, $n_taxid) = ($1, $2);
+	} else {
+	    $logger->error("Error on parsing line $nucl_line_number from nucleotide input file");
+	}
+
+	if ($nucl_line_number%$linenumberreport == 0)
+	{
+	    $logger->info("Got line $nucl_line_number from nucleotide input file");
+	}
+    }
+
+    # check if we have to read a new line from protein input
+    if (! $prot_line)
+    {
+	$prot_line = <PF>;
+	$prot_line_number++ if ($prot_line);
+
+	if ($prot_line =~ /^(\d+)\t(\d+)/)
+	{ 
+	    ($p_gi, $p_taxid) = ($1, $2);
+	} else {
+	    $logger->error("Error on parsing line $prot_line_number from protein input file");
+	}
+
+	if ($prot_line_number%$linenumberreport == 0)
+	{
+	    $logger->info("Got line $prot_line_number from protein input file");
+	}
+    }
+
+    # check if the gis are different
+    if ($n_gi && $p_gi && $n_gi == $p_gi)
+    {
+	$logger->logdie("Error: same gi for a nucleotide and a protein was found!");
+    }
+
+    # check if the current line in output is valid for one of the two input lines meaning the same as the gi!!!
+    my $output_string = $empty_line;
+
+    if ($output_line == $n_gi-1)
+    {
+	$output_string = pack($data_format, ($n_gi, $n_taxid));
+	($nucl_line, $n_gi, $n_taxid) = (undef, undef, undef);
+    } elsif ($output_line == $p_gi-1)
+    {
+	$output_string = pack($data_format, ($p_gi, $p_taxid));
+	($prot_line, $p_gi, $p_taxid) = (undef, undef, undef);
+    }
+
+    $out .= $output_string;
+    $output_line++;
+    if ($output_line%$linenumberreport == 0)
+    {
+	$logger->info("Wrote output line $output_line to file");
+    }
+
+    # run should be undef if we reached the end of both input files
+    $run = ! (eof(NF) && eof(PF));
+}
+
+$logger->info("Writing to output file started...");
+print OF $out;
+$logger->info("Writing to output file finished");
+
+# try to close all files
+close(OF) || $logger->logdie("Unable to close the outputfile '$outfile' after writing");
+close(NF) || $logger->logdie("Unable to close the nucleotid input file '$gi_taxid_nucl'");
+close(PF) || $logger->logdie("Unable to close the protein input file '$gi_taxid_prot'");
+
+$logger->info("Update process finished");
+
+exit;
+
+__END__
+
+
 my $act_line=0;
 
-my @lines = split(/\s+/, qx(tail -qn 1 gi_taxid_nucl.dmp gi_taxid_prot.dmp | cut -f 1 | tr "\n" " "));
+my @lines = split(/\s+/, qx(tail -qn 1   | cut -f 1 | tr "\n" " "));
 
 my @files=('gi_taxid_nucl.dmp', 'gi_taxid_prot.dmp');
 @files= reverse @files if ($lines[0]<$lines[1]);
