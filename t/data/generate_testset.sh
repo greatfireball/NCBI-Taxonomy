7de27ec8536738a3fae0067cfb830191594af7d5
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-10-12 18:04:46 +0200

    Adding different accession width into test set generator

--- t/data/generate_testset.sh
+++ t/data/generate_testset.sh
@@ -11,7 +11,7 @@ perl -e '
 
    for(my $i = 2; $i<2+$max_number; $i++)
    {
-      $acc=$header[rand(int(@header))].sprintf("%04d", $i);
+      $acc=$header[rand(int(@header))].sprintf("%0".int(rand(14)+3)."d", $i);
 
       print join("\t", ($acc, $acc.".".int(1+rand(10)), int(rand($max_number/5)+1), $i)),"\n";
    }' | gzip --best >test.accession2taxid.gz
